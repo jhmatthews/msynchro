374223531a65a63d31664fff1c9fc4bd41339eca
jhmatthews <jm8g08@soton.ac.uk>
2020-05-27 16:00:17 +0100

    added time step calculator

--- msynchro/evolve.py
+++ msynchro/evolve.py
@@ -76,3 +76,66 @@ def particle_evolve(energy_edges, energy_loss_rate, tloss_discrete, source, n_i,
 	n_iplusone = msynchro.tdma.TDMASolver(a, b, c, d)
 
 	return (n_iplusone)
+
+
+def get_dt(energies, loss_rate_cen, tloss_discrete, source, n_i):
+	'''
+	Evolve a particle distribution for one time step. any units allowed as
+	long as all energy and time units are consistent. Using a TDMA solver to evolve
+	the distribution following the method of Chiaberge & Chisellini 1999.
+
+	Parameters:
+		energy_edges 		array-like
+							edges of energy bins, len(N)
+
+		energy_loss_rate 	array-like or float
+							dE/dt at edges of energy bins, len (N)
+
+		tloss_discrete 		array-like or float
+							tau_loss in each bin, len (N-1)
+
+		source 				array-like
+							source term, len (N-1)
+
+		n_i 				array-like
+							len (N-1) array holding initial state
+
+		dt 					float
+							time-step, needs to have consistent units with
+							tloss_discrete and energy_loss_rate
+
+	Returns:
+		n_iplusone 			array-like
+							array holding the distribution at the next time step
+	'''
+	
+	if np.all(n_i == 0.0):
+		return (0.0)
+
+	minimum_n_i = np.max(n_i) * 1e-15
+	#print (np.max(n_i), minimum_n_i)
+	select = (n_i > minimum_n_i)
+
+	if np.isscalar(loss_rate_cen):
+		if loss_rate_cen == 0.0:
+			dndt1 = 0.0
+		else:
+			dndt1 = n_i[select] / energies[select] * loss_rate_cen
+	else:
+		dndt1 = n_i[select] / energies[select] * loss_rate_cen[select]
+
+	if np.isscalar(tloss_discrete):
+		if tloss_discrete == 0.0:
+			dndt2 = 0.0
+		else:
+			dndt2 = n_i[select] / tloss_discrete
+	else:
+		dndt2 = n_i[select] / tloss_discrete[select]
+
+	dt1 = np.nanmin(n_i[select] / dndt1)
+	dt2 = np.nanmin(n_i[select] / dndt2)
+
+
+	delta_t = 0.4 * np.nanmin( (dt1, dt2) )
+
+	return (delta_t)
